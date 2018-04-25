bc725d7bf41bf35183cd11e6a779f8427336d571
klenium <klenium2@gmail.com>
2018-04-25 16:04:12 +0200

    Create DeutschJozsaTest.java

---
+++ assets/code/DeutschJozsaTest.java
@@ -0,0 +1,25 @@
+package DeutschJozsaTest;
+
+import java.util.Random;
+import java.util.function.IntUnaryOperator;
+
+public class Main {
+    public static void main(String[] args) {
+        IntUnaryOperator function;
+        int constantValue = new Random().nextInt(2);
+        if (args[0].equals("constant"))
+			function = (x) -> constantValue;
+        else
+			function = (x) -> (x + constantValue) % 2;
+
+        int n = Integer.parseInt(args[1]);
+		
+        int result = 0;
+        for (int i = 0; i < n/2+1; ++i)
+            result += function.applyAsInt(i);
+		
+		boolean isConstant = (result == 0 || result == n/2+1);
+        String type = isConstant ? "constant" : "balanced";
+        System.out.println("Function is " + type + ".");
+    }
+}
